1d33272a9dfe95064a01645ead6bf39affcfa6b9
Haris Shabbir <05harisshabbir@gmail.com>
2026-05-16 01:10:56 +0500

    homepage style

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,15 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://heirloom-two-lake.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
